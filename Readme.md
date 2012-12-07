c482d2827950022aa3502ea52f08e95ef6377788
Matt Mueller <mattmuelle@gmail.com>
2012-12-07 09:48:37 -0800

    docs

--- Readme.md
+++ Readme.md
@@ -9,7 +9,82 @@
 
 ## API
 
-   
+### Helix()
+
+Create a Helix instance.
+
+```js
+var $ = Helix(el);
+```
+
+### $(selector)
+
+Select an element.
+
+```js
+$('.name')
+$('#nav')
+```
+
+### #[attr](val)
+
+Get and set attributes that are on the selected element.
+
+```html
+<input class="user" id="user" type="text" name="user">
+```
+
+```js
+$('.user').type('radio').name('person')
+```
+
+### #text(val)
+
+Get and set text
+
+```js
+$('.name').text('Matt')
+$('.name').text() // Matt
+```
+
+### Helix.html(val)
+
+Get and set HTML
+
+```js
+$('.name').text('<strong>Matt</strong>')
+$('.name').text() // <strong>Matt</strong>
+```
+
+### Helix.add(cls)
+
+Add a class
+
+```js
+$('.name').add('person')
+```
+
+### Helix.remove(cls)
+
+Remove a class
+
+```js
+$('.name').remove('person')
+```
+
+### Helix.toggle(cls)
+
+Toggle a class
+
+```js
+$('.name').toggle('person')
+```
+
+### Helix.attr(attr, val)
+
+Get and set attribute that doesn't already exist
+
+$('.name').attr('data-author', 'matt')
 
 ## License
 
---
+++ index.js
@@ -0,0 +1,149 @@
+/**
+ * Module dependencies
+ */
+
+var classes = require('classes');
+
+/**
+ * Export `Helix`
+ */
+
+module.exports = create;
+
+/**
+ * Create `Helix`
+ *
+ * @param {DOMNode} el
+ * @return {Function}
+ * @api public
+ */
+
+function create(el) {
+  return function(selector) {
+    return new Helix(selector, el);
+  };
+}
+
+/**
+ * Initialize `Helix`
+ *
+ * @param {string} selector
+ * @param {DOMNode} el
+ * @return {Helix}
+ * @api public
+ */
+
+function Helix(selector, el) {
+  this.el = el.querySelector(selector);
+  if (!this.el) throw new Error('cannot find ' + selector + ' selector');
+  this.classes = classes(this.el);
+
+  var attrs = this.el.attributes,
+      attr;
+
+  for (var i = 0, len = attrs.length; i < len; i++) {
+    attr = attrs.item(i).name;
+    this[attr] = this.setter(attr);
+  }
+}
+
+/**
+ * Get and set text
+ *
+ * @param {String} str
+ * @return {Helix}
+ * @api public
+ */
+
+Helix.prototype.text = function(str) {
+  if (!str) return this.el.innerText;
+  this.el.innerText = str;
+  return this;
+};
+
+/**
+ * Get and set HTML
+ *
+ *   $()
+ *
+ * @param {String} str
+ * @return {Helix}
+ * @api public
+ */
+
+Helix.prototype.html = function(str) {
+  if (!str) return this.el.innerHTML;
+  this.el.innerHTML = str;
+  return this;
+};
+
+/**
+ * Add class
+ *
+ * @param {String} cls
+ * @return {Helix}
+ * @api public
+ */
+
+Helix.prototype.add = function(cls) {
+  this.classes(this.el).add(cls);
+  return this;
+};
+
+/**
+ * Remove class
+ *
+ * @param {String} cls
+ * @return {Helix}
+ * @api public
+ */
+
+Helix.prototype.remove = function(cls) {
+  this.classes(this.el).remove(cls);
+  return this;
+};
+
+/**
+ * Toggle class
+ *
+ * @param {String} cls
+ * @return {Helix}
+ * @api public
+ */
+
+Helix.prototype.toggle = function(cls) {
+  this.classes(this.el).toggle(cls);
+  return this;
+};
+
+
+/**
+ * Get and set attribute
+ *
+ * @param {String} attr
+ * @param {Mixed} val
+ * @return {Helix}
+ * @api public
+ */
+
+Helix.prototype.attr = function(attr, val) {
+  if (!val) return this.el.getAttribute(attr);
+  this.el.setAttribute(attr, val);
+  return this;
+};
+
+/**
+ * Create setters to get and set attributes
+ *
+ * @param {String} name
+ * @return {Function}
+ * @api private
+ */
+
+Helix.prototype.setter = function(name) {
+  return function(val) {
+    if (!val) return this.el.getAttribute(name);
+    this.el.setAttribute(name, val);
+    return this;
+  };
+};
